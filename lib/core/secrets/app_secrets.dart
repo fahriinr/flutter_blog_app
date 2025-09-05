d10cea02b8531ebd9de3be03b78a73ccb93ef186
fahrii <fahrinurul05@gmail.com>
2025-09-06 01:11:32 +0700

    Fixing some bugs, remove api key, refactoring codes

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://zylomymnclqbakxuwtai.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bG9teW1uY2xxYmFreHV3dGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzMzMDMsImV4cCI6MjA3MTcwOTMwM30.w2H1x2aFo_T3WDS2z5vqEZ6tX2AFXZ54FpZb6nu_QUA';
-}
